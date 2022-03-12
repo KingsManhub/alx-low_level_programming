5a0624ee10d86be505484b100ceb5ec36f8bbc55
Ayodele Stephen <ayodammys@gmail.com>
2022-03-12 13:07:35 +0100

    a modified commit for 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+*main-entry point
+*Return: a return value of 0 since the entry point is int
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
