81c98c8379752d0b3438bae4fcef70f9a4c640a8
KingsManhub <ayodammys@gmail.com>
2022-03-22 05:31:36 -0700

    modified commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,12 +13,12 @@ int main(void)
 	for (num = 48; num <= 55; num++)
 	{
 		int incr_num;
-		
-		for (incr_num = num+1; incr_num <= 56; incr_num++)
+
+		for (incr_num = num + 1; incr_num <= 56; incr_num++)
 		{
 			int dou_incr_num;
-			
-			for ( dou_incr_num = incr_num + 1; dou_incr_num <= 57; dou_incr_num++)
+
+			for (dou_incr_num = incr_num + 1; dou_incr_num <= 57; dou_incr_num++)
 			{
 				putchar(num);
 				putchar(incr_num);
