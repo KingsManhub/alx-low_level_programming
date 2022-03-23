8ef69b5771084e9de4bccaefc77354e502b01ec2
KingsManhub <ayodammys@gmail.com>
2022-03-23 04:48:57 -0700

    modified commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha- Entry point
+ * @c: is a parameter
+ *
+ * Return: always 0 when function false or 1 when function true
+ *
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
