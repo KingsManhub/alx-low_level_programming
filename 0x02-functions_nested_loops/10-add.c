efc60a96ebf24c0127e3ae14b77c3711c4b31d6f
KingsManhub <ayodammys@gmail.com>
2022-03-23 11:10:45 -0700

    commit for add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add- Entry point
+ *
+ * @num1: parameter 1
+ * @num2: parameter 2
+ *
+ * Return: Always 0 (success).
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
