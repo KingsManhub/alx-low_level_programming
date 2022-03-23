1dcc05043161e7bb5a5f793735bea49c77d44496
KingsManhub <ayodammys@gmail.com>
2022-03-23 05:45:21 -0700

    modified 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs- Entry point
+ * @c: c is a parameter
+ *
+ * Return: always 0 (success)
+ *
+ */
+int _abs(int c)
+{
+	int result;
+
+	if (c < 0)
+	{
+		result = -1 * c;
+		return result;
+	}
+	else if (c == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return c;
+	}
+}
