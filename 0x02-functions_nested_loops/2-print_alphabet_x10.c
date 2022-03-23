56c76a0e8af864c175a91be56e250379eef65a06
KingsManhub <ayodammys@gmail.com>
2022-03-23 04:12:00 -0700

    commit for 2-print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Entry point
+ *
+ * Return: always 0 (success)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+}
