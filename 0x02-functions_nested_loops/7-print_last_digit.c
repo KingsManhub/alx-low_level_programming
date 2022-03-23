6e4e4b513c18f0000459e271107f6ca300d87199
KingsManhub <ayodammys@gmail.com>
2022-03-23 06:13:25 -0700

    modified commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,14 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = _abs(n);
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
-	last_digit = n % 10;
-	return (last_digit);
+	else
+	{
+		last_digit = n % 10;
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 }
