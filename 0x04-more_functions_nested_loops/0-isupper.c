217e262528ebdbddac8a38b8d6f0565a85b6ca92
Ayodele Stephen <ayodammys@gmail.com>
2022-07-25 14:45:05 +0100

    staging 0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,14 @@
 
 /**
  * _isupper - Check if paramter c represents an uppercase letter.
- * @c: Int representing a character                                                      * Return: Always 0.
+ * @c: Int representing a character
+ * Return: Always 0.
  */
-
 int _isupper(int c)
 {
-	if(c>=65 && c<= 89)
+	if (c >= 65 && c <= 90)
 	{
-		return 1;
+		return (1);
 	}
-	else
-	{
-		return 0;
-	}
+	return (0);
 }
