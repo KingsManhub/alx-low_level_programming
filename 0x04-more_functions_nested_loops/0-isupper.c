6ebe6bca203718ab479db62277830370eed0ef2f
Ayodele Stephen <ayodammys@gmail.com>
2022-07-25 14:42:21 +0100

    staging 0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character                                                      * Return: Always 0.
  */
 
 int _isupper(int c)
