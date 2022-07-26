59ce1d8b6444205df8676d03133ef8cdde76067c
Ayodele Stephen <ayodammys@gmail.com>
2022-07-26 17:37:57 +0100

    staging more_numbers file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 10 times the number from 0 - 14, following a new line.
+ *
+ * Return: Void
+ */
+
+void more_numbers(void)
+{
+	int count, value;
+
+	for (count = 1; count <= 10; ++count)
+	{
+		for (value = 0; value <= 14; ++value)
+		{
+			_putchar(value + '0');
+		}
+		_putchar('\n');
+	}
+}
